8203f27173508f032446d332c6c14ebe46a9decc
Hiromichi NOMATA <git@hiromichinomata.com>
2020-08-02 17:09:00 +0900

    [update] みたをおすと次の動画に進む

--- src/content_script.js
+++ src/content_script.js
@@ -1,4 +1,10 @@
 ﻿$(function () {
+  $('.status-button-1.viewing > span').on("click", function(){
+    // $('.video-arrow.next')[0].click();
+    if(!$('.status-button-1.viewing').hasClass('active')){
+      window.location.href = $('.video-arrow.next')[0].href;
+    }
+  });
     // ↓疎通確認コード：タイトルの■が回転するはず
     // console.log('Chrome拡張の疎通確認コードが動いています。動作確認ができたら削除してください。');
     // let flag = false;
